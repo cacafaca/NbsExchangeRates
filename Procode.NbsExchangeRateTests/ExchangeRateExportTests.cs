e8a8f7bcc333020af58c5acef04548c6f0135dfa
Nemanja Simović <Nemanja.simovic@gmail.com>
2017-01-25 03:22:57 +0100

    Tests.

---
+++ Procode.NbsExchangeRateTests/ExchangeRateExportTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Procode.NbsExchangeRate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Procode.NbsExchangeRate.Tests
+{
+    [TestClass()]
+    public class ExchangeRateExportTests
+    {
+        [TestMethod()]
+        public void Get_Exchange_Rate_For_31_12_2016()
+        {
+            string expected = "30.12.2016";
+            var s = ExchangeRateExport.GetExchangeRate(expected);
+
+            Assert.IsNotNull(s);
+            Assert.IsTrue(s.StartsWith("BrojKursneListe"));
+
+            var n = s.Split('\n', '\r');
+            Assert.IsTrue(n.Length > 1);
+            Assert.AreEqual(expected, s.Split('\n', '\r')[1].Split(',')[1]);
+
+        }
+
+        [TestMethod()]
+        public void Get_Today_Exchange_Rate_And_Get_Exchange_Rate_For_31_12_2016()
+        {
+            // 1.
+            var s1 = ExchangeRateExport.GetTodayExchangeRate();
+            Assert.IsNotNull(s1);
+            Assert.IsTrue(s1.StartsWith("BrojKursneListe"));
+
+            var n = s1.Split('\n', '\r');
+            Assert.IsTrue(n.Length > 1);
+            DateTime expected1 = DateTime.MinValue;
+            DateTime nbsOfficialRateChangeTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 8, 0, 0);
+            if (DateTime.Now >= nbsOfficialRateChangeTime)
+                expected1 = DateTime.Now;
+            else
+                expected1 = DateTime.Now.Subtract(new TimeSpan(1, 0, 0, 0)); // In case that I run test early in the morning, or at night.
+            Assert.AreEqual(expected1.ToString("dd.MM.yyyy"),
+                s1.Split('\n', '\r')[1].Split(',')[1]);
+
+            System.Threading.Thread.Sleep(3000);
+
+            // 2.
+            string expected2 = "30.12.2016";
+            var s2 = ExchangeRateExport.GetExchangeRate(expected2);
+
+            Assert.IsNotNull(s2);
+            Assert.IsTrue(s2.StartsWith("BrojKursneListe"));
+
+            var n2 = s2.Split('\n', '\r');
+            Assert.IsTrue(n2.Length > 1);
+            Assert.AreEqual(expected2, s2.Split('\n', '\r')[1].Split(',')[1]);
+
+        }
+    }
+}
---
+++ Procode.NbsExchangeRateTests/ExchangeRateTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Procode.NbsExchangeRate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Procode.NbsExchangeRate.Tests
+{
+    [TestClass()]
+    public class ExchangeRateTests
+    {
+        [TestMethod()]
+        public void Get_Todays_Exchange_Rate()
+        {
+            var er = new ExchangeRate();
+            var s = er.GetExchangeRate();
+
+            Assert.IsNotNull(s);
+            Assert.IsTrue(s.StartsWith("BrojKursneListe"));
+
+            var n = s.Split('\n', '\r');
+            Assert.IsTrue(n.Length > 1);
+            DateTime expected = DateTime.MinValue;
+            DateTime nbsOfficialRateChangeTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 8, 0, 0);
+            if (DateTime.Now >= nbsOfficialRateChangeTime)
+                expected = DateTime.Now;
+            else
+                expected = DateTime.Now.Subtract(new TimeSpan(1, 0, 0, 0)); // In case that I run test early in the morning, or at night.
+            Assert.AreEqual(expected.ToString("dd.MM.yyyy"),
+                s.Split('\n', '\r')[1].Split(',')[1]);
+        }
+
+        [TestMethod()]
+        public void Get_Rate_For_Day_09_01_2017()
+        {
+            DateTime expected = new DateTime(2017, 1, 9);
+            var er = new ExchangeRate();
+            var s = er.GetExchangeRate(expected);
+
+            Assert.IsNotNull(s);
+            Assert.IsTrue(s.StartsWith("BrojKursneListe"));
+
+            var n = s.Split('\n', '\r');
+            Assert.IsTrue(n.Length > 1);
+            Assert.AreEqual(expected.ToString("dd.MM.yyyy"),
+                s.Split('\n', '\r')[1].Split(',')[1]);
+        }
+    }
+}

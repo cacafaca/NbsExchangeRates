a4aac41a35bba22ba35adc6e350f2d41cc4980f3
Nemanja Simović <Nemanja.simovic@gmail.com>
2017-01-25 03:20:03 +0100

    Export as unmanaged functions.

---
+++ Procode.NbsExchangeRate/ExchangeRateExport.cs
@@ -0,0 +1,38 @@
+﻿using RGiesecke.DllExport;
+using System;
+
+namespace Procode.NbsExchangeRate
+{
+    public class ExchangeRateExport
+    {
+        [DllExport("GetTodayExchangeRate", CallingConvention = System.Runtime.InteropServices.CallingConvention.StdCall)]
+        public static string GetTodayExchangeRate()
+        {
+            return new ExchangeRate().GetExchangeRate();
+        }
+
+        [DllExport("GetExchangeRate", CallingConvention = System.Runtime.InteropServices.CallingConvention.StdCall)]
+        public static string GetExchangeRate(string date)
+        {
+            IFormatProvider fp = new MyFormat();
+            DateTime convertedDate;
+            if (DateTime.TryParseExact(date, "dd.MM.yyyy", fp, System.Globalization.DateTimeStyles.AssumeLocal, out convertedDate))
+            {
+                return new ExchangeRate().GetExchangeRate(convertedDate);
+            }
+            else
+            {
+                throw new ArgumentException(string.Format("Date '{0}' is not in correct format.", date));
+            }
+        }
+    }
+
+    public class MyFormat : IFormatProvider
+    {
+        public object GetFormat(Type formatType)
+        {
+            //throw new NotImplementedException();
+            return this;
+        }
+    }
+}

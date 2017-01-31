990c6a5f8562a5c62b077597a8d70052615254d6
Nemanja Simović <Nemanja.simovic@gmail.com>
2017-01-31 14:23:05 +0100

    - Lowered .Net version to 3.5. - Added console app to solution.

---
+++ Procode.NbsExchangeRate.ConsoleApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Procode.NbsExchangeRate.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExchangeRate er = new ExchangeRate();
+            string date = string.Empty;
+            DateTime parsedDate = DateTime.MinValue;
+            string cvs = string.Empty;
+
+            if (args.Length > 0)
+            {
+                date = args[0];
+                if (DateTime.TryParseExact(date, "dd.MM.yyyy", null, System.Globalization.DateTimeStyles.AssumeLocal, out parsedDate))
+                {
+                    try
+                    {
+                        cvs = er.GetExchangeRate(parsedDate);
+                    }
+                    catch (Exception ex)
+                    {
+                        System.Console.WriteLine("Can't read from NBS. Error: " + ex.Message);
+                        return;
+                    }
+                }
+                else
+                {
+                    System.Console.WriteLine("Expect date in format dd.MM.yyyy");
+                    return;
+                }
+            }
+            else
+            {
+                try
+                {
+                    cvs = er.GetExchangeRate();
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine("Can't read from NBS for todays date. Error: " + ex.Message);
+                    return;
+                }
+            }
+
+            System.Console.Write(cvs);
+        }
+    }
+}
--- Procode.NbsExchangeRate/EnumTypes.cs
+++ Procode.NbsExchangeRate/EnumTypes.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Procode.NbsExchangeRate
+﻿namespace Procode.NbsExchangeRate
 {
     public enum ReturnDataType
     {
